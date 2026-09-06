897dc91a5438cdb509e87caf0353da0198a7580f
Remi Adriaensen <46381050+SleepiDreamer@users.noreply.github.com>
2026-09-06 20:30:55 +0200

    chore: increase default bounces

--- include/renderer/StructsDX.h
+++ include/renderer/StructsDX.h
@@ -66,7 +66,7 @@ struct RenderSettings
 {
 	float maxFPS = 0.0f;
 	DebugMode debugMode = None;
-	uint32_t bounces = 2;
+	uint32_t bounces = 5;
 	float compSlider = 0.5f;
 	float skyIntensity = 1.0f;
 	float lightIntensity = 1.0f;
